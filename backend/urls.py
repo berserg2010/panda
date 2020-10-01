76dbd1a95a3eb1044ced7962a8f81505436bd469
berserg2010 <berserg2010@gmail.com>
2020-10-01 23:48:45 +0300

    Add add static templates for public_side

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -9,6 +10,8 @@ urlpatterns = [
 
     path('', include('public_side.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='public/not-found.html')
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- public_side/urls.py
+++ public_side/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-
-from .views import index
+from django.views.generic import TemplateView
 
 
 app_name = 'public_side'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', TemplateView.as_view(template_name='public/index.html'), name='index'),
+
+    path('process/', TemplateView.as_view(template_name='public/process.html'), name='process'),
+    path('faq/', TemplateView.as_view(template_name='public/faq.html'), name='faq'),
+    path('price/', TemplateView.as_view(template_name='public/price.html'), name='price'),
+    path('about/', TemplateView.as_view(template_name='public/about.html'), name='about'),
+    path('teachers/', TemplateView.as_view(template_name='public/teachers.html'), name='teachers'),
+    path('testimonials/', TemplateView.as_view(template_name='public/testimonials.html'), name='testimonials'),
+    path('contacts/', TemplateView.as_view(template_name='public/contacts.html'), name='contacts'),
 ]
