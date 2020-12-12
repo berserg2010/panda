ddcc97fe678ea18c847c0832f9b3a5d2f341a8d7
Sergei Zakamskov <berserg2010@gmail.com>
2020-12-12 22:21:40 +0300

    Fix account context_processor

--- backend/account/context_processors.py
+++ backend/account/context_processors.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AnonymousUser
 def get_account(request):
     user = request.user
 
-    if isinstance(user, AnonymousUser):
+    if isinstance(user, AnonymousUser) or user.is_superuser:
         account = None
     elif user.is_staff:
         account = user.teacher
