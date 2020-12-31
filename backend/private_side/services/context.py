0f4b50ff538cdba47bff8ea1d4188a9ab9799f6c
berserg2010 <berserg2010@gmail.com>
2020-12-31 08:28:04 +0300

    Fix get_free_lessons in IndexLkView

--- backend/private_side/services/context.py
+++ backend/private_side/services/context.py
@@ -5,7 +5,6 @@ from paid_course.models import FreeLesson
 def get_free_lessons(self):
     free_lessons = FreeLesson.objects.filter(
         get_user_context(self.request),
-        datetime__gte=date_now,
         finished=False,
     )
     return free_lessons
