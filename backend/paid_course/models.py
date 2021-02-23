e2bf74e3ea0dfd8869b92904b8240db2f332d3f5
Sergei Zakamskov <berserg2010@gmail.com>
2021-02-23 19:37:02 +0300

    Fix delete FreeLesson

--- backend/paid_course/models.py
+++ backend/paid_course/models.py
@@ -12,7 +12,7 @@ class BasicRelationshipTable(CommonId):
 
     finished = models.BooleanField(default=False, verbose_name='завершен')
 
-    teacher = models.ForeignKey(Teacher, null=True, blank=True, on_delete=models.PROTECT, verbose_name='учитель')
+    teacher = models.ForeignKey(Teacher, null=True, blank=True, on_delete=models.CASCADE, verbose_name='учитель')
     student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='ученик')
 
     class Meta:
