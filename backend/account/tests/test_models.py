0dd3f831d1eff97536ccf121b8e6420322fda060
Sergei Zakamskov <berserg2010@gmail.com>
2021-04-09 23:18:26 +0300

    Fix payment save and test

--- backend/account/tests/test_models.py
+++ backend/account/tests/test_models.py
@@ -1,62 +1,77 @@
 import pytest
 from mixer.backend.django import mixer
+from datetime import datetime
+
 from django.utils import timezone
 
 from common.utils import date_now
 from course.models import Course
+from ..services.payment import get_valid_until
 from ..models import Student, Payment
 
 
 pytestmark = pytest.mark.django_db
 
 
+def get_order_time(days: int) -> datetime:
+    order_time = date_now() - timezone.timedelta(days=days)
+    return order_time
+
+
 class TestPayment:
-
     def test_save(self):
-
         student = mixer.blend(Student)
         course = mixer.blend(Course)
 
-        order_time = date_now() - timezone.timedelta(days=84)
+        order_time = get_order_time(84)
         instance = mixer.blend(
             Payment,
             student=student,
             order_time=order_time,
-            valid_until=order_time + timezone.timedelta(days=28),
+            valid_until=mixer.SKIP,
             group_of_course=course.group_of_course,
         )
         assert Payment.objects.count() == 1
         assert instance.first_payment is None
+        assert instance.valid_until == get_valid_until(order_time)
 
-        order_time = date_now() - timezone.timedelta(days=42)
+        instance.save()
+        assert Payment.objects.count() == 1
+        assert instance.first_payment is None
+
+        order_time = get_order_time(42)
         first_payment = mixer.blend(
             Payment,
             student=student,
             order_time=order_time,
-            valid_until=order_time + timezone.timedelta(days=28),
+            valid_until=get_valid_until(order_time),
             group_of_course=course.group_of_course,
         )
         assert Payment.objects.count() == 2
         assert first_payment.first_payment is None
 
-        order_time = date_now() - timezone.timedelta(days=28)
+        instance.save()
+        assert Payment.objects.count() == 2
+        assert instance.first_payment is None
+
+        order_time = get_order_time(28)
         instance = mixer.blend(
             Payment,
             student=student,
             order_time=order_time,
-            valid_until=order_time + timezone.timedelta(days=28),
+            valid_until=get_valid_until(order_time),
             group_of_course=course.group_of_course,
         )
         assert Payment.objects.count() == 3
         assert instance.first_payment is not None
         assert instance.first_payment == first_payment
 
-        order_time = date_now() - timezone.timedelta(days=14)
+        order_time = get_order_time(14)
         instance = mixer.blend(
             Payment,
             student=student,
             order_time=order_time,
-            valid_until=order_time + timezone.timedelta(days=28),
+            valid_until=get_valid_until(order_time),
             group_of_course=course.group_of_course,
         )
         assert Payment.objects.count() == 4
